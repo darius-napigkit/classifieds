6b7d3c055c16e5150e2d29d8643011730ead16e9
Darius Napigkit <darius.napigkit@gmail.com>
2021-01-07 18:10:04 -0800

    Initial commit.

---
+++ src/test/java/com/metheor/java/samples/classifieds/ClassifiedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metheor.java.samples.classifieds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassifiedsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
